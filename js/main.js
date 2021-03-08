ea934cbe04efaf2a40e155432077dcd36df30863
Maria Belen Trigueros <bel.trigueros@gmail.com>
2021-03-08 20:53:47 -0300

    Agregue la primera introduccion al juego. Inventario y opciones de acuerdo a los objetos que tenes.

--- js/main.js
+++ js/main.js
@@ -1,61 +1,87 @@
-// /*  Situación ejemplo: el usuario tiene que elegir una opción en el juego, y eso desemboca en distintas consecuencias.
-// */
-// //Variables: defino las opciones y sus consecuencias.
-// let opciones = ["Sacar el celular", "Tratar de esconderme"];
-// let consecuencias = ["'Ahi estas! Te descubrimos!' Tal vez hubiera sido mejor mantenerse en la oscuridad...", "No hay lugar para esconderse... a correr!"];
-// //Mensaje introductorio y primera propuesta al usuario
-// let entradaUsuario = parseInt(prompt("No recuerdo nada de las últimas horas, no sé dónde estoy... está todo oscuro. Opciones: 1 - Sacar el celular 2 - Tratar de esconderme 0 - Salir" ));
-// //Bucle interactivo: se verifica la elección del usuario 
-// while(entradaUsuario > 0){
-//     if (entradaUsuario <= 2){
-//         alert(opcionElegida(entradaUsuario));
-//     } else {
-//         alert("No elegiste ninguna opcion, acaso no queres ver como sigue la historia?");
-//     }
-//     break;
-// }
-// //Función para definir la elección 
-// function opcionElegida(numeroElegido){
-//     //Transformamos la elección en el índice de la lista
-//     let indice = numeroElegido - 1;
-//     //Retornamos la consecuencia de dicha eleccion
-//     return consecuencias[indice];
-// }
+// Variables
 
-
-// Situacion ejemplo 2: Inventario. El jugador tiene un inventario con objetos, y elige si leer su diario o no.
+let opciones = [];
+let consecuencias = [];
+const inventario = [];
 
 // Clase Inventario
-class Inventario {
-    constructor (diario, celular){
-        this.diario = diario;
-        this.celular = celular;
+
+class Celular {
+    constructor(bateria){
+        this.bateria = parseInt(bateria);
+        this.signal = false;
     }
-    leerDiario(){
-        alert("Fecha 10/01: Hoy voy a ir al bosque a acampar con mis amigos. Hace mucho que no los veo, así que tengo ganas de pasar el día con ellos en la naturaleza...");
+    cargar(){
+        this.bateria = this.bateria++;
+    }
+    signalFound(){
+        this.signal = true;
+    }
 }
+
+inventario.push(new Celular(0));
+
+// INTRODUCCION
+
+alert("Abrís los ojos... estás en una especie de bosque, te sentís desorientado. Está anocheciendo, y no te acordás nada de las últimas horas. Vas a tener que empezar a caminar, y buscar indicios que te ayuden a recordar lo que pasó... lo único que tenés con vos es tu mochila, con un celular sin batería.");
+
+// //Mensaje introductorio y primera propuesta al usuario
+let entradaUsuario = parseInt(prompt("Estás caminando a través del bosque, mirando con atención, y al lado de un árbol encontrás tirados un cuchillo y una linterna. Dado el reducido espacio de tu mochila, solo podés agarrar uno... 1 - Linterna 2 - Cuchillo 0 - Salir")); 
+
+// Agrego las opciones y las consecuencias a los arrays correspondientes.
+
+opciones.push("Linterna");
+opciones.push("Cuchillo");
+
+consecuencias.push("Elegiste la linterna, seria conveniente guardarla para cuando oscuresca...");
+consecuencias.push("Elegiste el cuchillo, tenelo en acceso rapido en caso de alguna urgencia.");
+
+// Clase Arma
+
+class Arma {
+    constructor(nombre, potencia){
+        this.nombre = nombre;
+        this.potencia = parseInt(potencia);
+    }
 }
 
-// Creo el objeto Inventario
-const inventario = new Inventario("diario", "celular");
+// Clase Linterna
 
-// Variables
-let opciones = ["Leer el diario", "No leer el diario"];
-let consecuencias = ["Elegiste leer el diario, esto te ayudara a encontrar pistas respecto a lo que paso.", "Preferiste no leer el diario, para intentar recordar de otra manera sin preconceptos."];
+class Linterna{
+    constructor(pila){
+        this.pila = parseInt(pila);
+        this.encendida = false;
+    }
+    encender(){
+        this.encendida = true;
+    }
+}
 
-let entradaUsuario = parseInt(prompt("Encuentro un diario en mi mochila. Esto puede ser util para recordar las ultimas horas, aunque tambien puede que influya en mi memoria y me confunda... que deberia hacer? 1 - Leer el diario 2 - No leerlo  0 - Salir" ));
 // //Bucle interactivo: se verifica la elección del usuario 
+// De acuerdo al objeto elegido, este se agrega al inventario. 
+
 while(entradaUsuario > 0){
     if (entradaUsuario == 1){
     alert(opcionElegida(entradaUsuario));
-    inventario.leerDiario();
+    inventario.push(new Linterna(100));
+    console.log(inventario);
     }
     else if (entradaUsuario == 2){
     alert(opcionElegida(entradaUsuario));
+    inventario.push(new Arma("Cuchillo", 5));
+    console.log(inventario);
     } 
     else {
     alert("No elegiste ninguna opcion, acaso no queres ver como sigue la historia?");
 }
+// Aca depende de lo que elegiste, vas a poder avanzar o tener que continuar x otro camino. Para lograr esto, usando FIND busco en el inventario por el objeto que necesito.
+    alert("Continuas caminando, y te encontras con una hierba alta que te impide seguir...");
+    if(inventario.find(elemento => elemento.nombre == "Cuchillo")){
+                alert("Como escogiste el cuchillo, podes abrirte camino entre la maleza y continuar por este rumbo.");
+            }
+            else{
+                alert("Vas a tener que tomar el camino a tu izquierda, que se ve mas oscuro y peligroso... por suerte tenes la linterna!");
+            } 
 break;
 }
 
@@ -66,3 +92,6 @@ let indice = numeroElegido - 1;
     //Retornamos la consecuencia de dicha eleccion
 return consecuencias[indice];
 }
+
+
+
