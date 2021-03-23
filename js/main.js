7bd4132825c50d2e9c329ab3c0a911228ecff990
Maria Belen Trigueros <bel.trigueros@gmail.com>
2021-03-22 21:31:45 -0300

    Modifique los elementos cuchillo y linterna del inventario, para poder usar sus metodos, asociandolos con evento click y dblclick. Guardo informacion de las opciones elegidas en el LocalStorage, y salen por consola.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 // Variables
 
 let opciones = [];
@@ -21,8 +22,12 @@ class Herramienta {
         else if(this.bateria != 0){
             this.uso = true;
         }
-    this.bateria--;
+    this.bateria--;}
+
+    dejarDeUsar(){
+        this.uso = false;
     }
+    
 }
 
 // Agrego elemento "Celular" de tipo Herramienta al inventario.
@@ -39,11 +44,11 @@ let intro = document.createElement("p");
 intro.innerHTML = "Abrís los ojos... estás en una especie de bosque, te sentís desorientado. Está anocheciendo, y no te acordás nada de las últimas horas. Vas a tener que empezar a caminar, y buscar indicios que te ayuden a recordar lo que pasó... lo único que tenés con vos es tu mochila, con un celular sin batería."
 contenedorJuego.appendChild(intro);
 
-//Mensaje introductorio y primera propuesta al usuario
+//Mensaje introductorio y primera propuesta al usuario.
 
-let primeraOpcion = document.createElement("p");
-primeraOpcion.innerHTML = "Estás caminando a través del bosque, mirando con atención, y al lado de un árbol encontrás tirados un cuchillo y una linterna. Dado el reducido espacio de tu mochila, solo podés agarrar uno..."; 
-contenedorJuego.appendChild(primeraOpcion);
+let primeraSituacion = document.createElement("p");
+primeraSituacion.innerHTML = "Estás caminando a través del bosque, mirando con atención, y al lado de un árbol encontrás tirados un cuchillo y una linterna. Dado el reducido espacio de tu mochila, solo podés agarrar uno..."; 
+contenedorJuego.appendChild(primeraSituacion);
 
 // Agrego las opciones desde la clase Herramienta y creo los botones a partir del nombre de los objetos.
 
@@ -51,9 +56,19 @@ opciones.push(new Herramienta (datos_herramientas[1]));
 opciones.push(new Herramienta (datos_herramientas[2]));
 console.log(opciones);
 
+// Agrego los strings correspondientes al array de consecuencias.
+
+consecuencias.push("Elegiste la linterna, seria conveniente guardarla para cuando oscuresca...");
+consecuencias.push("Elegiste el cuchillo, tenelo en acceso rapido en caso de alguna urgencia.");
+console.log(consecuencias);
+
+// Creo las opciones como elementos usando la funcion crearBoton.
+
 for (const opcion of opciones){
     crearBoton(opcion);
 }
+
+// Creo el Inventario como elemento en el HTML con el celular previamente agregado.
 
 crearElementos(inventario[0]);
 
@@ -67,59 +82,120 @@ function crearBoton(opcion){
     contenedorJuego.appendChild(objeto);
 }
 
+//Asocio los botones a eventos onclick, desencadena en una consecuencia y el objeto elegido se agrega al inventario. Se eliminan los objetos que no seran utilizados.
+
 let cuchillo_btn = document.getElementById("cuchillo");
 cuchillo_btn.onclick = () => {
-    let consecuenciaDos = document.createElement("p");
-    consecuenciaDos.innerHTML= "Elegiste el cuchillo, tenelo en acceso rapido en caso de alguna urgencia.";
+    crearConsecuencias(consecuencias[1]);
     inventario.push(new Herramienta (datos_herramientas[2]));
     crearElementos(inventario[1]);
     console.log(inventario);
-    contenedorJuego.appendChild(consecuenciaDos);
+
+    // Almaceno el elemento elegido en el Almacenamiento Local.
+
+    const almacenado_cuc = JSON.stringify(datos_herramientas[2]);
+    localStorage.setItem("cuchillo", almacenado_cuc);
+
+    // Obtengo el mismo y lo veo por consola. La idea es luego poder usar esta informacion como punto de guardado. 
+
+    const almacenado_cuc2 = JSON.parse(localStorage.getItem("cuchillo"));
+    console.log(almacenado_cuc2);
+
     linterna_btn.parentNode.removeChild(linterna_btn);
     intro.parentNode.removeChild(intro);
-    primeraOpcion.parentNode.removeChild(primeraOpcion);
+    primeraSituacion.parentNode.removeChild(primeraSituacion);
     cuchillo_btn.parentNode.removeChild(cuchillo_btn);
 
-    opcionDos();
+    segundaSituacion();
+
+    // Esto me permite, al hacer click, usar el elemento cuchillo del inventario. Para dejar de usarlo, hago doble click.
     
+    let cuchillo_inventario = document.getElementById("cuchillo_li");
+    cuchillo_inventario.onclick = () => {
+    inventario[1].usar();
+    if(inventario[1].uso == true){
+        if(inventario[1].uso == true){
+            onmousemove = e => {
+                cuchillo_inventario.style =`position: absolute;
+                                            top: ${e.pageY-25}px; 
+                                            left: ${e.pageX-25}px;`
+            }
+        }
+    }
+            cuchillo_inventario.ondblclick = () => {
+                inventario[1].dejarDeUsar();
+                onmousemove = () => {
+                cuchillo_inventario.style =`position: relative;`
+            }
+        }
+    }
 }
 
 let linterna_btn = document.getElementById("linterna");
 linterna_btn.onclick = () => {
-    let consecuenciaUno = document.createElement("p");
-    consecuenciaUno.innerHTML= "Elegiste la linterna, seria conveniente guardarla para cuando oscuresca...";
+    crearConsecuencias(consecuencias[0]);
     inventario.push( new Herramienta (datos_herramientas[1]));
     crearElementos(inventario[1]);
     console.log(inventario);
-    contenedorJuego.appendChild(consecuenciaUno);
+
+// Almaceno el elemento elegido en el Almacenamiento Local.
+
+    const almacenado_lin = JSON.stringify(datos_herramientas[1]);
+    localStorage.setItem("linterna", almacenado_lin);
+
+// Obtengo el mismo y lo veo por consola. La idea es luego poder usar esta informacion como punto de guardado.
+
+    const almacenado_lin2 = JSON.parse(localStorage.getItem("linterna"));
+    console.log(almacenado_lin2);
+
     cuchillo_btn.parentNode.removeChild(cuchillo_btn);
     intro.parentNode.removeChild(intro);
-    primeraOpcion.parentNode.removeChild(primeraOpcion);
+    primeraSituacion.parentNode.removeChild(primeraSituacion);
     linterna_btn.parentNode.removeChild(linterna_btn);
 
-    opcionDos();
-}
+    segundaSituacion();
 
+    // Esto me permite, al hacer click, usar el elemento linterna del inventario. Para dejar de usarlo, hago doble click.
 
-// Funcion para crear la segunda opcion, la misma va a depender del objeto elegido en la instancia anterior.
-
-function opcionDos (){
-
-
-    let segundaOpcion = document.createElement("p");
-    segundaOpcion.innerHTML = "Continuas caminando, y te encontras con una hierba alta que te impide seguir..."; 
-    contenedorJuego.appendChild(segundaOpcion);
-    if(inventario.find(elemento => elemento.nombre == "Cuchillo")){
-        let consecuenciaTres = document.createElement("p");
-        consecuenciaTres.innerHTML = "Como escogiste el cuchillo, podes abrirte camino entre la maleza y continuar por este rumbo.";
-        contenedorJuego.appendChild(consecuenciaTres);
-        }
-        else if (inventario.find(elemento => elemento.nombre == "Linterna")){ 
-            let consecuenciaCuatro = document.createElement("p");
-            consecuenciaCuatro.innerHTML = "Vas a tener que tomar el camino a tu izquierda, que se ve mas oscuro y peligroso... por suerte tenes la linterna!";  
-            contenedorJuego.appendChild(consecuenciaCuatro);
+    let linterna_inventario = document.getElementById("linterna_li");
+    linterna_inventario.onclick = () => {
+    inventario[1].usar(); 
+    if(inventario[1].uso == true){
+        onmousemove = e => {
+            linterna_inventario.style =`position: absolute;
+                                        color: $colorFondo;
+                                        top: ${e.pageY-25}px; 
+                                        left: ${e.pageX-25}px;
+                                        box-shadow: 1px 0px 80px rgb(255, 255, 255); `
             }
         }
+    }
+        linterna_inventario.ondblclick = () => {
+            inventario[1].dejarDeUsar();
+            onmousemove = () => {
+            linterna_inventario.style =`position: relative;`
+            }
+        }
+    }
+
+// Funcion para crear la segunda situacion, la misma va a depender del objeto elegido en la instancia anterior.
+
+function segundaSituacion (){
+
+    let situacionDos = document.createElement("p");
+    situacionDos.innerHTML = "Continuas caminando, y te encontras con una hierba alta que te impide seguir..."; 
+    contenedorJuego.appendChild(situacionDos);
+    if(inventario.find(elemento => elemento.nombre == "Cuchillo")){
+        consecuencias.push("Como escogiste el cuchillo, podes abrirte camino entre la maleza y continuar por este rumbo.");
+        crearConsecuencias(consecuencias[2]);
+        console.log(consecuencias);
+        }
+            else if (inventario.find(elemento => elemento.nombre == "Linterna")){ 
+                consecuencias.push("Vas a tener que tomar el camino a tu izquierda, que se ve mas oscuro y peligroso... por suerte tenes la linterna!");
+                crearConsecuencias(consecuencias[2]);    
+                console.log(consecuencias);
+        }
+    }
 
 
 // Funcion para crear los elementos del inventario. 
@@ -129,6 +205,15 @@ function crearElementos (elemento){
     let objeto = document.createElement("li");
     objeto.id = elemento.nombre.toLowerCase() + "_li"; 
     objeto.innerHTML = `<img src= ${elemento.imagen} alt= "Esto es un placeholder del ${elemento.nombre}">
-                        <p>${elemento.nombre}</p>`;
+                        `;
     inventarioJuego.appendChild(objeto);
+}
+
+// Funcion para crear las consecuencias.
+
+function crearConsecuencias (con){
+    let consecuencia = document.createElement("p");
+    consecuencia.id = "consecuencia_" + con;
+    consecuencia.innerHTML = `${con}`;
+    contenedorJuego.appendChild(consecuencia);
 }
